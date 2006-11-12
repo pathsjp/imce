c4c22980c8fa09986ea92053fea2ea2541882ed1
ufku <ufku@9910.no-reply.drupal.org>
2006-11-12 21:21:06 +0000

    browser URL correction for some multi-site configurations.

--- imce_set_fck.js
+++ imce_set_fck.js
@@ -4,15 +4,13 @@ if (Drupal.jsEnabled) {
 }
 function imceInitiateFCK() {
   if ("undefined" != typeof(window.FCKeditorAPI)) {
-    var path;
     var width = 640;
     var height = 480;
     var types = ['Image', 'Link', 'Flash'];
     for (var i in FCKeditorAPI.__Instances) {
       var fck = FCKeditorAPI.__Instances[i];
-      path = path ? path : fck.Config.BasePath.substring(0, fck.Config.BasePath.indexOf('modules')) + 'index.php?q=imce/browse';
       for (var t in types) {
-        eval('fck.Config.'+types[t]+'BrowserURL = path; fck.Config.'+types[t]+'BrowserWindowWidth = width; fck.Config.'+types[t]+'BrowserWindowHeight = height;');
+        eval('fck.Config.'+types[t]+'BrowserURL = imceBrowserURL; fck.Config.'+types[t]+'BrowserWindowWidth = width; fck.Config.'+types[t]+'BrowserWindowHeight = height;');
       }
     }
   }
