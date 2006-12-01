e20f9b9dcbbf483c2eb25b5201fccb339692fea5
ufku <ufku@9910.no-reply.drupal.org>
2006-12-01 19:55:30 +0000

    Image and link insertions are separated. now it's possible to insert a link to an image.

--- imce_set_inline.js
+++ imce_set_inline.js
@@ -3,12 +3,13 @@ if (Drupal.jsEnabled) {
   $(imceInitiateInline);
 }
 
-var imceActiveTextarea;
+var imceActiveTextarea, imceActiveType;
 function imceInitiateInline() {
   $('a.imce-insert-inline').each( function () {
     $(this.parentNode).css('display', 'block');
     $(this).click(function() {
-      imceActiveTextarea = $('#'+this.name).get(0);
+      imceActiveTextarea = $('#'+this.name.split('|')[0]).get(0);
+      imceActiveType = this.name.split('|')[1];
       window.open(this.href, '_imce_', 'width=640, height=480, resizable=1');
       return false;
     });
@@ -17,7 +18,9 @@ function imceInitiateInline() {
 
 //custom callback. hook:ImceFinish
 function _imce_ImceFinish(path, w, h, imceWin) {
-  var html = w&&h ? ('<img src="'+path+'" width="'+w+'" height="'+h+'" alt="'+path.substr(path.lastIndexOf('/')+1)+'" />') : ('<a href="'+path+'">'+path.substr(path.lastIndexOf('/')+1)+'</a>');
+  var basename = path.substr(path.lastIndexOf('/')+1);
+  imceActiveType = imceActiveType ? imceActiveType : (w&&h ? 'image' : 'link');
+  var html = imceActiveType=='image' ? ('<img src="'+ path +'" width="'+ w +'" height="'+ h +'" alt="'+ basename +'" />') : ('<a href="'+ path +'">'+ basename +'</a>');
   imceInsertAtCursor(imceActiveTextarea, html);
   imceWin.close();
 }
