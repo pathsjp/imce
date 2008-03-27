9c906b2fbde9a729d28bc0aac6e09804bd0accda
ufku <ufku@9910.no-reply.drupal.org>
2008-03-27 09:56:42 +0000

    - integration function runs delayed to process new instances. - tinymce v3 support

--- imce_set_tinymce.js
+++ imce_set_tinymce.js
@@ -1,24 +1,29 @@
 // $Id$
-if (Drupal.jsEnabled) {
-  $(window).load(imceInitiateTinyMCE);
-}
-function imceInitiateTinyMCE() {
+
+function imceInitiateTinyMCE(cycle) {
   if ("undefined" != typeof(window.tinyMCE)) {
-    for (var i=0; i<tinyMCE.configs.length; i++) {
-      tinyMCE.configs[i]['file_browser_callback'] = 'imceImageBrowser';
+    if (tinyMCE.configs) for (var i in tinyMCE.configs) {
+      tinyMCE.configs[i].file_browser_callback = 'imceImageBrowser';
     }
-    setTimeout('for (var i in tinyMCE.instances) {var fE = tinyMCE.instances[i]["formElement"]; if (fE["tagName"]=="DIV") tinyMCE.instances[i]["formElement"] = fE.firstChild;}', 3000);//fix formElement issue of tinymce after 3sec. comment out this line if you use tinymce2.1 or above.
+    else if (tinyMCE.editors) for (var i in tinyMCE.editors) {//v3
+      tinyMCE.editors[i].settings.file_browser_callback = 'imceImageBrowser';
+    }
   }
+
+  var cycle = (cycle || 0)+1;
+  if (cycle < 5) setTimeout('imceInitiateTinyMCE('+ cycle +')', 2000);
 }
+
 var imceTinyWin, imceTinyField, imceTinyType, imceTinyURL;
 function imceImageBrowser(field_name, url, type, win) {
   //if (type!='image') return;//work for only images
   var width = 640;
   var height = 480;
-  var imcePopup = window.open(imceBrowserURL, '', 'width='+ width +', height='+ height +', resizable=1');
-  imcePopup.focus();
+  window.open(imceBrowserURL||'/?q=imce/browse', '', 'width='+ width +', height='+ height +', resizable=1').focus();
   imceTinyWin = win;
   imceTinyField = win.document.forms[0].elements[field_name];
   imceTinyType = type;
   imceTinyURL = url;
 }
+
+$(document).ready(imceInitiateTinyMCE);
