a27c7cefc52cef4ba796505543ed4ffed6c1c01a
ufku <ufku@9910.no-reply.drupal.org>
2009-02-03 23:21:26 +0000

    - added ability to pass an onload function by url.

--- js/imce_set_app.js
+++ js/imce_set_app.js
@@ -2,38 +2,42 @@
 //When imce url contains &app=appName|fileProperty1@correspondingFieldId1|fileProperty2@correspondingFieldId2|...
 //the specified fields are filled with the specified properties of the selected file.
 
-var appFields = {};
+var appFields = {}, appWindow = (top.appiFrm||window).opener;
 
 //execute when imce loads.
 imce.hooks.load.push(function(win) {
   var data = decodeURIComponent(location.href.substr(location.href.lastIndexOf('app=')+4)).split('|');
-  var appname = data.shift();
+  var appName = data.shift();
+  //run custom onload function if avaliable.
+  if (data[0].indexOf('@') < 0 && $.isFunction(appWindow[data[0]])) {
+    return appWindow[data[0]](win);
+  }
+  //set send to
+  imce.setSendTo(Drupal.t('Send to @app', {'@app': appName}), appFinish);
+  //extract fields
   for (var i in data) {
     var arr = data[i].split('@');
     appFields[arr[0]] = arr[1];
   }
-  //set send to
-  imce.setSendTo(Drupal.t('Send to @app', {'@app': appname}), appFinish);
   //highlight file
   if (appFields['url']) {
-    var doc = (top.appiFrm||win).opener.document;
-    if (appFields['url'].indexOf(',')) {
+    if (appFields['url'].indexOf(',') > -1) {
       var arr = appFields['url'].split(',');
       for (var i in arr) {
-        if ($('#'+ arr[i], doc).size()) {
+        if ($('#'+ arr[i], appWindow.document).size()) {
           appFields['url'] = arr[i];
           break;
         }
       }
     }
-    var filename = $('#'+ appFields['url'], doc).val();
+    var filename = $('#'+ appFields['url'], appWindow.document).val();
     imce.highlight(filename.substr(filename.lastIndexOf('/')+1));
   }
 });
 
 //sendTo function
 var appFinish = function(file, win) {
-  var win = top.appiFrm||win, doc = $(win.opener.document);
+  var doc = $(appWindow.document);
   for (var i in appFields) {
     doc.find('#'+ appFields[i]).val(file[i]);
   }
@@ -41,6 +45,6 @@ var appFinish = function(file, win) {
     try{doc.find('#'+ appFields['url']).blur().change().focus()}catch(e){};
     try{doc.find('#'+ appFields['url']).trigger('onblur').trigger('onchange').trigger('onfocus')}catch(e){};//inline events
   }
-  win.opener.focus();
+  appWindow.focus();
   win.close();
 };
