281b004271685cd5e31d4677d74d85d88aa063ff
ufku <ufukbay@gmail.com>
2016-03-01 01:17:41 +0200

    Added Link button for CKEditor.

--- js/plugins/ckeditor/imce.ckeditor.js
+++ js/plugins/ckeditor/imce.ckeditor.js
@@ -9,6 +9,7 @@
   CKEDITOR.plugins.add('imce', {
     // Define commands and buttons
     init: function (editor) {
+      // Image
       editor.addCommand('imceimage', {
         exec: CKEDITOR.imce.imageDialog
       });
@@ -16,6 +17,15 @@
         label: CKEDITOR.imce.imageLabel(),
         command: 'imceimage',
         icon: editor.config.ImceImageIcon
+      });
+      // Link
+      editor.addCommand('imcelink', {
+        exec: CKEDITOR.imce.linkDialog
+      });
+      editor.ui.addButton('ImceLink', {
+        label: CKEDITOR.imce.linkLabel(),
+        command: 'imcelink',
+        icon: editor.config.ImceLinkIcon
       });
     }
   });
@@ -36,29 +46,56 @@
      * Opens Imce for inserting images into CKEditor.
      */
     imageDialog: function (editor) {
+      return CKEDITOR.imce.openDialog(editor, 'image');
+    },
+
+    /**
+     * Returns link button label.
+     */
+    linkLabel: function () {
+      return Drupal.t('Insert file links using Imce File Manager');
+    },
+
+    /**
+     * Opens Imce for inserting links into CKEditor.
+     */
+    linkDialog: function (editor) {
+      return CKEDITOR.imce.openDialog(editor, 'link');
+    },
+
+    /**
+     * Opens Imce for inserting images or links into CKEditor.
+     */
+    openDialog: function (editor, type) {
       var width = Math.min(1000, parseInt(screen.availWidth * 0.8));
       var height = Math.min(800, parseInt(screen.availHeight * 0.8));
-      var url = CKEDITOR.imce.url('sendto=CKEDITOR.imce.imageSendto&type=image&ck_id=' + encodeURIComponent(editor.name));
+      var url = CKEDITOR.imce.url('sendto=CKEDITOR.imce.sendto&type=' + type + '&ck_id=' + encodeURIComponent(editor.name));
       editor.popup(url, width, height);
     },
 
     /**
-     * Imce sendto handler for image selection.
+     * Imce sendto handler for image/link dialog.
      */
-    imageSendto: function (File, win) {
+    sendto: function (File, win) {
       var imce = win.imce;
       var editor = CKEDITOR.instances[imce.getQuery('ck_id')];
       if (editor) {
         var i;
         var lines = [];
         var selection = imce.getSelection();
+        var is_img = imce.getQuery('type') === 'image';
         for (i in selection) {
           if (!imce.owns(selection, i)) {
             continue;
           }
           File = selection[i];
-          if (File.width) {
+          // Image
+          if (is_img && File.width) {
             lines.push('<img src="' + File.getUrl() + '" width="' + File.width + '" height="' + File.height + '" alt="' + File.formatName() + '" />');
+          }
+          // Link
+          else {
+            lines.push('<a href="' + File.getUrl() + '">' + File.formatName() + '</a>');
           }
         }
         editor.insertHtml(lines.join('<br />'));
