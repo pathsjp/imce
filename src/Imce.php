0a669aac33b18bc576412bdaeb93ef4fd4746ae4
ufku <ufukbay@gmail.com>
2016-11-28 19:55:24 +0300

    Give admin profile to user#1

--- src/Imce.php
+++ src/Imce.php
@@ -44,10 +44,13 @@ class Imce {
     if (!isset($profile)) {
       // Check stream wrapper
       if ($wrapper = \Drupal::service('stream_wrapper_manager')->getViaScheme($scheme)) {
+        $storage = \Drupal::entityTypeManager()->getStorage('imce_profile');
+        if ($user->id() == 1 && $profile = $storage->load('admin')) {
+          return $profile;
+        }
         $imce_settings = \Drupal::config('imce.settings');
         $roles_profiles = $imce_settings->get('roles_profiles', array());
         $user_roles = array_flip($user->getRoles());
-        $storage = \Drupal::entityTypeManager()->getStorage('imce_profile');
         foreach ($roles_profiles as $rid => $profiles) {
           if (isset($user_roles[$rid]) && !empty($profiles[$scheme])) {
             if ($profile = $storage->load($profiles[$scheme])) {
