9c1441b5cd5f6cc25fdda189c8464d13330e8041
thallesvf <thallesvf@3589086.no-reply.drupal.org>
2019-09-18 10:46:29 -0300

    Issue #3082001 by thalles: Create test for getProfileOptions method on ImceSettingsForm

--- tests/src/Kernel/Form/ImceSettingsFormTest.php
+++ tests/src/Kernel/Form/ImceSettingsFormTest.php
@@ -4,6 +4,7 @@ namespace Drupal\Tests\imce\Kernel\Form;
 
 use Drupal\imce\Form\ImceSettingsForm;
 use Drupal\KernelTests\KernelTestBase;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
 
 /**
  * Kernel tests for ImceSettingsForm.
@@ -12,10 +13,13 @@ use Drupal\KernelTests\KernelTestBase;
  */
 class ImceSettingsFormTest extends KernelTestBase {
 
+  use StringTranslationTrait;
+
   protected $imceSettingsForm;
 
   public static $modules = [
     'system',
+    'imce',
   ];
 
   protected function setUp() {
@@ -30,6 +34,12 @@ class ImceSettingsFormTest extends KernelTestBase {
   public function testFormId() {
     $this->assertTrue(is_string($this->imceSettingsForm->getFormId()));
     $this->assertEquals('imce_settings_form', $this->imceSettingsForm->getFormId());
+  }
+
+  public function testProfileOptions() {
+    $options = $this->imceSettingsForm->getProfileOptions();
+    $this->assertTrue(is_array($options));
+    $this->assertArraySubset($options, ['' => '-' . $this->t('None') . '-']);
   }
 
 }
