7b9e5d75a0b07ba292e01a86eeafab0267d2821b
thallesvf <thallesvf@3589086.no-reply.drupal.org>
2019-09-26 14:18:41 -0300

    Issue #3084021 by thalles: Create kernel test for getConfirmText in ImceProfileDeleteForm

--- tests/src/Kernel/Form/ImceProfileDeleteFormTest.php
+++ tests/src/Kernel/Form/ImceProfileDeleteFormTest.php
@@ -4,6 +4,7 @@ namespace Drupal\Tests\imce\Kernel\Form;
 
 use Drupal\KernelTests\KernelTestBase;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
 use Drupal\Core\Url;
 use Drupal\imce\Form\ImceProfileDeleteForm;
 
@@ -19,7 +20,7 @@ class ImceProfileDeleteFormTest extends KernelTestBase {
   /**
    * The form delete profile.
    *
-   * @var object
+   * @var \Drupal\imce\Form\ImceProfileDeleteForm
    */
   protected $profileDeleteForm;
 
@@ -43,6 +44,13 @@ class ImceProfileDeleteFormTest extends KernelTestBase {
     $this->assertTrue(is_string($url->toString()));
     $this->assertSame('/admin/config/media/imce', $url->toString());
     $this->assertEquals('/admin/config/media/imce', $url->toString());
+  }
+
+  public function testConfirmText() {
+    $confirmText = $this->profileDeleteForm->getConfirmText();
+    $this->assertInstanceOf(TranslatableMarkup::class, $confirmText);
+    $this->assertTrue(is_string($confirmText->__toString()));
+    $this->assertEqual($this->t('Delete'), $confirmText);
   }
 
 }
