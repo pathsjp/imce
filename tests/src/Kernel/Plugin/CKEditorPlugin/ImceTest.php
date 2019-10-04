2cb240a79fe8ca12b04612dcb1573f3695c02fe2
thallesvf <thallesvf@3589086.no-reply.drupal.org>
2019-10-04 14:57:33 -0300

    Issue #3085911 by thalles: Create method getPluginDefination

--- tests/src/Kernel/Plugin/CKEditorPlugin/ImceTest.php
+++ tests/src/Kernel/Plugin/CKEditorPlugin/ImceTest.php
@@ -4,7 +4,10 @@ namespace Drupal\Tests\imce\Kernel\Plugin\CKEditorPlugin;
 
 use Drupal\KernelTests\KernelTestBase;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\editor\Entity\Editor;
+use Drupal\imce\Plugin\CKEditorPlugin\Imce;
 use Drupal\Tests\user\Traits\UserCreationTrait;
+use Drupal\text\Plugin\Field\FieldWidget\TextareaWithSummaryWidget;
 
 /**
  * Kernel tests for Imce plugins for CKEditor.
@@ -15,6 +18,13 @@ class ImceTest extends KernelTestBase {
 
   use StringTranslationTrait;
   use UserCreationTrait;
+
+  /**
+   * The Imce ckeditor plugin.
+   *
+   * @var \Drupal\imce\Plugin\CKEditorPlugin\Imce
+   */
+  public $imce;
 
   /**
    * Modules to enable.
@@ -32,7 +42,20 @@ class ImceTest extends KernelTestBase {
    */
   protected function setUp() {
     parent::setUp();
+    $this->imce = new Imce([], "text_textarea_with_summary", $this->getPluginDefinations());
+  }
 
+  public function getPluginDefinations() {
+    return [
+      "field_types" => [
+        0 => "text_with_summary",
+      ],
+      "multiple_values" => FALSE,
+      "id" => "text_textarea_with_summary",
+      "label" => $this->t("Text area with a summary"),
+      "class" => TextareaWithSummaryWidget::class,
+      "provider" => "text",
+    ];
   }
 
 }
