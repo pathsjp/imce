1e90e1f808a9224497297bdf86c719a8b2f4b879
thallesvf <thallesvf@3589086.no-reply.drupal.org>
2019-11-01 14:33:25 -0300

    Issue #3091837 by thalles: add methods in abstract class KernelTestBasePlugin

--- tests/src/Kernel/Plugin/KernelTestBasePlugin.php
+++ tests/src/Kernel/Plugin/KernelTestBasePlugin.php
@@ -30,4 +30,47 @@ abstract class KernelTestBasePlugin extends KernelTestBase {
     'imce',
   ];
 
+  public function getConf() {
+    return [
+      "extensions" => "*",
+      "maxsize" => '104857600.0',
+      "quota" => 0,
+      "maxwidth" => 0,
+      "maxheight" => 0,
+      "replace" => 0,
+      "thumbnail_style" => "",
+      "folders" => [
+        "." => [
+          "permissions" => [
+            "all" => TRUE,
+          ],
+        ],
+      ],
+      "pid" => "admin",
+      "scheme" => "public",
+      "root_uri" => "public://",
+      "root_url" => "/sites/default/files",
+      "token" => "Vof6182Y9jbV1jFfCU0arR2XDI8qs-OfO8c-R-IbkTg",
+    ];
+  }
+
+  /**
+   * Get plugins definations.
+   *
+   * @return array
+   *   Return plugins definations.
+   */
+  public function getPluginDefinations() {
+    return [
+      "field_types" => [
+        0 => "text_with_summary",
+      ],
+      "multiple_values" => FALSE,
+      "id" => "text_textarea_with_summary",
+      "label" => $this->t("Text area with a summary"),
+      "class" => TextareaWithSummaryWidget::class,
+      "provider" => "text",
+    ];
+  }
+
 }
