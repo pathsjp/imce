e0e0e2b4fc3b869ac384e270e1a11126da61226a
thallesvf <thallesvf@3589086.no-reply.drupal.org>
2019-11-04 14:17:42 -0300

    Issue #3092284 by thalles: Create initial setup to ImcePlugin/NewFolderTest

---
+++ tests/src/Kernel/Plugin/ImcePlugin/NewFolderTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Drupal\Tests\imce\Kernel\Plugin\ImcePlugin;
+
+use Drupal\imce\ImceFM;
+use Drupal\imce\Plugin\ImcePlugin\Newfolder;
+use Drupal\Tests\imce\Kernel\Plugin\KernelTestBasePlugin;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Kernel tests for Imce plugins for Imce Plugin NewFolder.
+ *
+ * @group imce
+ */
+class NewFolderTest extends KernelTestBasePlugin {
+
+  /**
+   * The Imce ckeditor plugin.
+   *
+   * @var \Drupal\imce\Plugin\ImcePlugin\Newfolder
+   */
+  public $newFolder;
+
+  /**
+   * The Imce file manager.
+   *
+   * @var \Drupal\imce\ImceFM
+   */
+  public $imceFM;
+
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  public static $modules = [
+    'user',
+    'system',
+    'imce',
+  ];
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+    $this->newFolder = new Newfolder([], 'newfolder', $this->getPluginDefinations());
+    // $this->imceFM = new ImceFM($this->getConf(), \Drupal::currentUser(), Request::create("/imce"));
+  }
+
+  public function test() {
+    $this->assertEquals('test', 'test');
+  }
+
+}
