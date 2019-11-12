d0025df4eb759df8fadc3e3d7bdaa0157aea541a
thallesvf <thallesvf@3589086.no-reply.drupal.org>
2019-11-12 18:26:17 -0300

    Issue #3094015 by thalles: Create initial setup to ImcePlugin/UploadTest

---
+++ tests/src/Kernel/Plugin/ImcePlugin/UploadTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Drupal\Tests\imce\Kernel\Plugin\ImcePlugin;
+
+use Drupal\imce\ImceFM;
+use Drupal\imce\Plugin\ImcePlugin\Upload;
+use Drupal\Tests\imce\Kernel\Plugin\KernelTestBasePlugin;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Kernel tests for Imce plugins for Imce Plugin Core.
+ *
+ * @group imce
+ */
+class UploadTest extends KernelTestBasePlugin {
+
+  /**
+   * The Imce ckeditor plugin.
+   *
+   * @var \Drupal\imce\Plugin\ImcePlugin\Upload
+   */
+  public $upload;
+
+  /**
+   * The Imce file manager.
+   *
+   * @var \Drupal\imce\ImceFM
+   */
+  public $imceFM;
+
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  public static $modules = [
+    'user',
+    'system',
+    'imce',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+    $this->upload = new Upload([], "text_textarea_with_summary", $this->getPluginDefinations());
+    $this->imceFM = new ImceFM($this->getConf(), \Drupal::currentUser(), Request::create("/imce"));
+  }
+
+  /**
+   * This method will be removed.
+   */
+  public function test() {
+    $this->assertEquals('test', 'test');
+  }
+
+}
