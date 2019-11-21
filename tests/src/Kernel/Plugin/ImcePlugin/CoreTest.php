10298d059f0a207a287d3a1ce45406695d24443b
thallesvf <thallesvf@3589086.no-reply.drupal.org>
2019-11-21 15:10:52 -0300

    Issue #3096041 by thalles: Fix CoreTest::getConf()

--- tests/src/Kernel/Plugin/ImcePlugin/CoreTest.php
+++ tests/src/Kernel/Plugin/ImcePlugin/CoreTest.php
@@ -105,25 +105,7 @@ class CoreTest extends KernelTestBasePlugin {
    */
   public function getConf() {
     return [
-      "extensions" => "*",
-      "maxsize" => '104857600.0',
-      "quota" => 0,
-      "maxwidth" => 0,
-      "maxheight" => 0,
-      "replace" => 0,
-      "thumbnail_style" => "",
-      "folders" => [
-        "." => [
-          "permissions" => [
-            "all" => TRUE,
-          ],
-        ],
-      ],
-      "pid" => "admin",
-      "scheme" => "public",
-      "root_uri" => "public://",
-      "root_url" => "/sites/default/files",
-      "token" => "Vof6182Y9jbV1jFfCU0arR2XDI8qs-OfO8c-R-IbkTg",
+      'permissions' => ['all' => TRUE],
     ];
   }
 
