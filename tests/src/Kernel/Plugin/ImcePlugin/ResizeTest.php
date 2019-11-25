0498faaa699fb88a44e0f6911d0e4e33bb4b8512
thallesvf <thallesvf@3589086.no-reply.drupal.org>
2019-11-25 17:25:23 -0300

    Issue #3096693 by thalles: Redefine method ResizeTest::getPluginDefinations()

--- tests/src/Kernel/Plugin/ImcePlugin/ResizeTest.php
+++ tests/src/Kernel/Plugin/ImcePlugin/ResizeTest.php
@@ -52,6 +52,11 @@ class ResizeTest extends KernelTestBasePlugin {
   protected function setUp() {
     parent::setUp();
     $this->imceFM = $this->getImceFM();
+    $this->resize = new Resize([], 'resize', $this->getPluginDefinations());
+    $this->setParametersRequest();
+    $this->setActiveFolder();
+
+    $this->resize->opResize($this->imceFM);
   }
 
   /**
@@ -85,6 +90,22 @@ class ResizeTest extends KernelTestBasePlugin {
       'height' => '281',
       'copy' => '1',
     ]);
+  }
+
+  /**
+   * Get plugins definations to new folder.
+   */
+  public function getPluginDefinations() {
+    return [
+      'weight' => 0,
+      'operations' => [
+        'resize' => 'opResize',
+      ],
+      'id' => 'resize',
+      'label' => 'Resize',
+      'class' => 'Drupal\imce\Plugin\ImcePlugin\Resize',
+      'provider' => 'imce',
+    ];
   }
 
 }
