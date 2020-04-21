e51f887e321e251da06b645d499932a520499663
thallesvf <thallesvf@3589086.no-reply.drupal.org>
2020-04-21 16:26:32 -0300

    Issue #3129715 by thalles: Create plugins.js

---
+++ imce_modal/js/plugin.js
@@ -0,0 +1,13 @@
+/**
+ * @file
+ * Drupal File plugin.
+ *
+ * @ignore
+ */
+
+(function ($, Drupal, drupalSettings, CKEDITOR) {
+  "use strict";
+
+  CKEDITOR.plugins.add('imceModal', {});
+
+})(jQuery, Drupal, drupalSettings, CKEDITOR);
