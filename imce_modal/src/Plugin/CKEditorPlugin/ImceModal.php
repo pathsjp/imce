91368db7eedf05205f2d6fed6dd689b2656ffb01
thallesvf <thallesvf@3589086.no-reply.drupal.org>
2020-04-21 17:17:26 -0300

    Issue #3129719 by thalles: Implements methods of CKEditorPluginConfigurableInterface in ImceModal

--- imce_modal/src/Plugin/CKEditorPlugin/ImceModal.php
+++ imce_modal/src/Plugin/CKEditorPlugin/ImceModal.php
@@ -4,6 +4,7 @@ namespace Drupal\imce_modal\Plugin\CKEditorPlugin;
 
 use Drupal\ckeditor\CKEditorPluginBase;
 use Drupal\ckeditor\CKEditorPluginConfigurableInterface;
+use Drupal\Core\Form\FormStateInterface;
 use Drupal\editor\Entity\Editor;
 
 /**
@@ -16,14 +17,14 @@ use Drupal\editor\Entity\Editor;
  */
 class ImceModal extends CKEditorPluginBase implements CKEditorPluginConfigurableInterface {
 
-  // /**
-  //  * {@inheritdoc}
-  //  */
-  // public function getFile() {
-  //   if ($module_path = drupal_get_path('module', 'imce_modal')) {
-  //     return $module_path . '/js/plugin.js';
-  //   }
-  // }
+  /**
+   * {@inheritdoc}
+   */
+  public function getFile() {
+    if ($module_path = drupal_get_path('module', 'imce_modal')) {
+      return $module_path . '/js/plugin.js';
+    }
+  }
 
   /**
    * {@inheritdoc}
@@ -37,13 +38,38 @@ class ImceModal extends CKEditorPluginBase implements CKEditorPluginConfigurable
   /**
    * {@inheritdoc}
    */
+  public function getConfig(Editor $editor) {
+    return [];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   public function getButtons() {
     return [
-      'bgimage' => [
+      'imceModal' => [
         'label' => $this->t('IMCE Modal File Browser'),
         'image' => drupal_get_path('module', 'imce_modal') . '/js/plugins/ckeditor/icons/image-file-16.png',
       ],
     ];
+  }
+
+  /**
+   * {@inheritdoc}
+   *
+   * @see \Drupal\editor\Form\EditorFileDialog
+   * @see ckeditor_bgimage_upload_settings_form()
+   */
+  public function settingsForm(array $form, FormStateInterface $form_state, Editor $editor) {
+    $form['iframeModal'] = [
+      '#type' => 'inline_template',
+      '#template' => '<iframe with="100%" src="{{ url }}"></iframe>',
+      '#context' => [
+        'url' => '/en/imce?sendto=CKEDITOR.imce.sendto&type=image&ck_id=edit-body-0-value',
+      ],
+    ];
+
+    return $form;
   }
 
 }
