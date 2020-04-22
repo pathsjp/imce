7b2eb06f1db6a983f5a283c9b343a5552c2e36eb
thallesvf <thallesvf@3589086.no-reply.drupal.org>
2020-04-22 15:42:59 -0300

    Issue #3130048 by thalles: Add IMCE Modal button in CKEditor

--- imce_modal/js/plugin.js
+++ imce_modal/js/plugin.js
@@ -8,6 +8,15 @@
 (function ($, Drupal, drupalSettings, CKEDITOR) {
   "use strict";
 
-  CKEDITOR.plugins.add('imceModal', {});
+  CKEDITOR.plugins.add('imceModal', {
+    init: function (editor) {
+      editor.ui.addButton('imceModal', {
+        label: 'IMCE Modal File Browser',
+        command: 'imceModal',
+        toolbar: 'insert',
+        icon: this.path + 'plugins/ckeditor/icons/image-file-16.png'
+      });
+    }
+  });
 
 })(jQuery, Drupal, drupalSettings, CKEDITOR);
