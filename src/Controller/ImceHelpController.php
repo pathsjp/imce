0303a22128f121710beacb7d523690711c7bcc5f
thallesvf <thallesvf@3589086.no-reply.drupal.org>
2020-04-30 16:51:13 -0300

    Issue #3132635 by thalles: Remove unsed statement in ImceHelpController

--- src/Controller/ImceHelpController.php
+++ src/Controller/ImceHelpController.php
@@ -6,7 +6,6 @@ use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Extension\ModuleExtensionList;
 use Drupal\Core\Routing\RouteMatchInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
-use Drupal\help\HelpSectionManager;
 
 /**
  * Controller routines for help routes.
@@ -21,13 +20,6 @@ class ImceHelpController extends ControllerBase {
   protected $routeMatch;
 
   /**
-   * The help section plugin manager.
-   *
-   * @var \Drupal\help\HelpSectionManager
-   */
-  protected $helpManager;
-
-  /**
    * The list of available modules.
    *
    * @var \Drupal\Core\Extension\ModuleExtensionList
@@ -39,14 +31,11 @@ class ImceHelpController extends ControllerBase {
    *
    * @param \Drupal\Core\Routing\RouteMatchInterface $route_match
    *   The current route match.
-   * @param \Drupal\help\HelpSectionManager $help_manager
-   *   The help section manager.
    * @param \Drupal\Core\Extension\ModuleExtensionList $extension_list_module
    *   The list of available modules.
    */
-  public function __construct(RouteMatchInterface $route_match, HelpSectionManager $help_manager, ModuleExtensionList $extension_list_module) {
+  public function __construct(RouteMatchInterface $route_match, ModuleExtensionList $extension_list_module) {
     $this->routeMatch = $route_match;
-    $this->helpManager = $help_manager;
     $this->extensionListModule = $extension_list_module;
   }
 
@@ -56,7 +45,6 @@ class ImceHelpController extends ControllerBase {
   public static function create(ContainerInterface $container) {
     return new static(
       $container->get('current_route_match'),
-      $container->get('plugin.manager.help_section'),
       $container->get('extension.list.module')
     );
   }
