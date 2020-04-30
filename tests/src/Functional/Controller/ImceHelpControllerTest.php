761d7fb8c746350a1493691157113f73edf413ad
thallesvf <thallesvf@3589086.no-reply.drupal.org>
2020-04-30 17:02:21 -0300

    Issue #3132680 by thalles: Create a functional test to imce\Controller\ImceHelpController

---
+++ tests/src/Functional/Controller/ImceHelpControllerTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Drupal\Tests\imce\Functional\Controller;
+
+use Drupal\Core\Url;
+use Drupal\Tests\BrowserTestBase;
+
+/**
+ * Provides automated tests for the imce module.
+ *
+ * @group imce
+ */
+class ImceHelpControllerTest extends BrowserTestBase {
+
+  /**
+   * {@inheritDoc}
+   */
+  protected $defaultTheme = 'stable';
+
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  public static $modules = ['imce'];
+
+  /**
+   * A user with permission to administer site configuration.
+   *
+   * @var \Drupal\user\UserInterface
+   */
+  protected $user;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+    $this->user = $this->drupalCreateUser(['administer imce']);
+    $this->drupalLogin($this->user);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getInfo() {
+    return [
+      'name' => "imce ImceBrowserController's controller functionality",
+      'description' => 'Test Unit for module imce and controller ImceHelpController.',
+      'group' => 'imce',
+    ];
+  }
+
+  /**
+   * Tests imce functionality.
+   */
+  public function testImceBrowserController() {
+    $this->drupalGet(Url::fromRoute('imce.help'));
+    $this->assertSession()->statusCodeEquals(200);
+  }
+
+}
