4120fade2a68884146723a705b4656a81a512903
thallesvf <thallesvf@3589086.no-reply.drupal.org>
2020-06-17 18:38:22 -0300

    Issue #1594434 by thalles, Kingdutch: Improve file select feedback

--- js/imce.js
+++ js/imce.js
@@ -580,8 +580,9 @@ send: function (fid) {
 setSendTo: function (title, func) {
   imce.send = function (fid) { fid && func(imce.fileGet(fid), window);};
   var opFunc = function () {
-    if (imce.selcount != 1) return imce.setMessage(Drupal.t('Please select a file.'), 'error');
-    imce.send(imce.vars.prvfid);
+    if (imce.validateSelCount(1, 1)) {
+      imce.send(imce.vars.prvfid);
+    }
   };
   imce.vars.prvtitle = title;
   return imce.opAdd({name: 'sendto', title: title, func: opFunc});
