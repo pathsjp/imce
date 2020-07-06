6d4362db93439f30d0e2767b865857c9ee0312f1
paulocs <paulocs@3640109.no-reply.drupal.org>
2020-07-06 17:39:10 -0300

    Issue #3024769 by paulocs, geoffmen, thalles: Column widths issues

--- tpl/imce-file-list.tpl.php
+++ tpl/imce-file-list.tpl.php
@@ -23,7 +23,7 @@ $imce =& $imce_ref['imce'];
 if ($imce['perm']['browse'] && !empty($imce['files'])) {
   foreach ($imce['files'] as $name => $file) {?>
   <tr id="<?php print $raw = rawurlencode($file['name']); ?>">
-    <td class="name"><?php print $raw; ?></td>
+    <td class="name" title="<?php print $raw; ?>"><?php print $raw; ?></td>
     <td class="size" id="<?php print $file['size']; ?>"><?php print format_size($file['size']); ?></td>
     <td class="width"><?php print $file['width']; ?></td>
     <td class="height"><?php print $file['height']; ?></td>
