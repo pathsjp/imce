8b50c708714850b1078c6527d2d22ed7e037bc55
thallesvf <thallesvf@3589086.no-reply.drupal.org>
2020-07-20 17:57:15 -0300

    Issue #3160397 by thalles: Create trait to image compress

---
+++ src/ImceImageCompressTrait.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Drupal\imce;
+
+/**
+ * Image compress trait.
+ */
+trait ImceImageCompressTrait {
+
+
+  public static function compressTinify($uri, $tinifyKey) {
+    \Tinify\setkey($tinifyKey);
+    \Tinify\fromFile($uri)->toFile($uri);
+  }
+
+  public function compressGd($uri) {
+    $info = getimagesize($uri);
+
+    if ($info['mime'] == 'image/jpeg') {
+      $image = imagecreatefromjpeg($uri);
+    }
+    elseif ($info['mime'] == 'image/gif') {
+      $image = imagecreatefromgif($uri);
+    }
+    elseif ($info['mime'] == 'image/png') {
+      $image = imagecreatefrompng($uri);
+    }
+    else {
+      die('Unknown image file format');
+    }
+
+    // Compress and save file to jpg.
+    imagejpeg($image, $uri, 90);
+
+    // Return destination file.
+    return $uri;
+  }
+
+  public function compressImagick($uri) {
+
+    $imagePath = \Drupal::service('file_system')->realpath($uri);
+    $img = new \Imagick();
+    $img->readImage($imagePath);
+    $img->setImageCompression(\Imagick::COMPRESSION_JPEG2000);
+    $img->setImageCompressionQuality(80);
+    $img->stripImage();
+    $img->writeImage($imagePath);
+  }
+}
